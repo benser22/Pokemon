ebfc92de92b671d6956e2fec7a3ecd557e296bd3
Benjamín Serrano Friedlander <benser22@hotmail.com>
2023-08-20 04:03:07 -0300

    404 error fixes

--- client/src/components/Error404/Error404.jsx
+++ client/src/components/Error404/Error404.jsx
@@ -18,7 +18,7 @@ const Error404 = () => {
   return (
     <div className={styles.error}>
       <h1 className={styles.myh1}>404</h1>
-      <h2 className={styles.myh2}>Page not found</h2>
+      <h2 className={styles.myh2}>Error: page not found</h2>
       <p className={styles.myp}>Sorry, the page you are looking for does not exist.</p>
       <button className={styles.mybutton} onClick={handleHomeClick} title="Back to Home">
         <FaHome />
